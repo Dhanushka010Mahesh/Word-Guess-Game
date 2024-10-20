5d721010182beef20e754912326c2d9a4b5ea4c3
Dhanushka010Mahesh <E2240438@bit.uom.lk>
2024-10-20 11:59:33 +0530

    only have design and api check

--- app/src/main/java/com/e2240438/wordguess/WordGuessActivity.java
+++ app/src/main/java/com/e2240438/wordguess/WordGuessActivity.java
@@ -19,44 +19,58 @@ public class WordGuessActivity extends AppCompatActivity {
     private String ranWords="m";
     private double point=100;
     private boolean wordLenGet=false;
-    private int guessTime=0;
-    private int wordNumber=0;
+    private int guessTime,wordNumber=0;
+    TextView disName,disMark,disPoint;
+    EditText getUserGuessWord;
+    Button smilerWord,submitBtn,getLen;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_guess);
+        //text box
+        disName = findViewById(R.id.txtDisplayName);
+        disMark = findViewById(R.id.txtDisplayMarks);
+        disPoint=findViewById(R.id.txtDisplayPoints);
+        getUserGuessWord=findViewById(R.id.txtGuessWord);
+        //button
+        smilerWord=findViewById(R.id.btnRhymes);
+        submitBtn=findViewById(R.id.btnSubmitGuessWord);
+        getLen=findViewById(R.id.btnGetLength);
 
-
-        TextView disName = findViewById(R.id.txtDisplayName);
-        TextView disMark = findViewById(R.id.txtDisplayMarks);
-        TextView disPoint=findViewById(R.id.txtDisplayPoints);
-        EditText getUserGuessWord=findViewById(R.id.txtGuessWord);
-
+        //display open activity
         disName.setText(MainActivity.player.getName());
         disMark.setText(String.valueOf(MainActivity.player.getMark()));
         disPoint.setText(String.valueOf(point));
 
+        //first randem word genarate
         ranWords=ranWordGenarate();
-        Toast.makeText(WordGuessActivity.this, ranWords, Toast.LENGTH_SHORT).show();
-        Button smilerWord=findViewById(R.id.btnRhymes);
-        Button submitBtn=findViewById(R.id.btnSubmitGuessWord);
+
+        //enter word and click submit button
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //get guess word varible after cut emptry space
                 String wordGuess = getUserGuessWord.getText().toString().trim();
 
-                if (point > 5) {
+                //one word guess wroung subtract 10 point. so check have point 10 or grater than
+                if (point >=10) {
+                    //edit text can not be submit emptry
                     if (!wordGuess.isEmpty()) {
                         if (wordGuess.equalsIgnoreCase(ranWords)) {
+                            //correct guess mark and 10 and give new word
                             double newPoint = MainActivity.player.getMark() + 10;
                             MainActivity.player.setPoint(newPoint);
                             disMark.setText(String.valueOf(MainActivity.player.getMark()));
                             Toast.makeText(WordGuessActivity.this, "Correct Guess!", Toast.LENGTH_SHORT).show();
                             wordNumber++;
+                            //get anther randem number when user give correct word
                             ranWords=ranWordGenarate();
                         } else {
+                            //incorrect guess point will subtract 10
                             guessTime++;
+                            //five time guess after aler can get Similar word App = cap ,map ,nap
                             if(guessTime>=5){
                                 smilerWord.setText("Smilar word(enable)");
                             }
@@ -65,13 +79,17 @@ public class WordGuessActivity extends AppCompatActivity {
                             Toast.makeText(WordGuessActivity.this, "Guess word is invalid", Toast.LENGTH_SHORT).show();
                         }
                     } else {
+                        //if user give emtry value
                         Toast.makeText(WordGuessActivity.this, "Please enter a guess word", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    Toast.makeText(WordGuessActivity.this, "No more points! Starting new round.", Toast.LENGTH_SHORT).show();
+                    //10 time give player cant guess word give anther word but not addition mark
+                    Toast.makeText(WordGuessActivity.this, "No more points! Let try new word again.", Toast.LENGTH_SHORT).show();
                     point = 100;
+                    disPoint.setText(String.valueOf(point));
                     guessTime = 0;
                     wordLenGet=false;
+                    ranWords=ranWordGenarate();
                     smilerWord.setText("Smilar word(disable)");
                 }
 
@@ -79,7 +97,7 @@ public class WordGuessActivity extends AppCompatActivity {
             }
         });
 
-        Button getLen=findViewById(R.id.btnGetLength);
+        //player can one time get word length but subtract 5 point
         getLen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -95,6 +113,7 @@ public class WordGuessActivity extends AppCompatActivity {
         });
 
 
+        //after guess 5 time can get similar word but subtract 5 point
         smilerWord.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -109,14 +128,14 @@ public class WordGuessActivity extends AppCompatActivity {
 
     }
 
+    //genarate similar word method
     public static String getSimilarWord(String word){
         String apiKey = "";  /* Replace with your API key
-        zZ21Eu8d7K+C4XQMp0EHzg==OL7tejgzcAMMVE8BDM */
-        String wordToRhyme = word;  // The word you want to find rhymes for
+        zZ21Eu8d7K+C4XQMp0EHzg==OL7tejgzcAMMVE8BDM  */
+        String wordToRhyme = word;
         String wordArr="";
 
         try {
-            // API URL with the word as a query parameter
             URL url = new URL("https://api.api-ninjas.com/v1/rhyme?word=" + wordToRhyme);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
@@ -153,6 +172,7 @@ public class WordGuessActivity extends AppCompatActivity {
         }
     }
 
+    //randem word genarate method
     public String ranWordGenarate(){
         String randomWord="";
         try {
